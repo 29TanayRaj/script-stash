1d9ab15275e3916cbedf516db7ee65b6fe13cefd
Tanay Raj <161963809+29TanayRaj@users.noreply.github.com>
2025-02-02 21:55:54 +0530

    Create youtube_transcript.py
    Python function for fetching youtube video transcript.

---
+++ youtube_transcript.py
@@ -0,0 +1,29 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+import re 
+
+# Function to fetch transcript of a youtube video.
+def get_transcript(vid_code):
+
+    """
+    Function to get trancript of a youtube video
+    
+    Parameters-
+    vid_code: str type, code form the youtube video. e.g In this URL 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', 'dQw4w9WgXcQ' is the video code.
+
+    Returns transcipt of the video in a str format
+    """
+
+    # Getting the subtitles from the video
+    vid_sub = YouTubeTranscriptApi.get_transcript('nNK4xKkAapM')
+
+    # Vid_sub is a list of dicts, we only need text from each dict
+
+    transcript = ''
+    for dict in vid_sub:
+        transcript = transcript + ' '+ dict['text']
+
+    # removing the new line '\n' symbol
+
+    transcript = re.sub("\n",'',transcript)
+
+    return transcript
